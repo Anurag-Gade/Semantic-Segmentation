b59971edadaeb92b033460b237b354db844bed44
Anurag Gade <88252632+Anurag-Gade@users.noreply.github.com>
2023-06-15 19:13:27 +0530

    Update dataloader.py

--- segmentation_networks/segnet/dataloader.py
+++ segmentation_networks/segnet/dataloader.py
@@ -4,6 +4,8 @@ import_frameworks()
 def dataloader():
   X_train = []
   y_train = []
+  X_val = []
+  y_val = []
   X_test = []
   y_test = []
 
@@ -14,7 +16,6 @@ def dataloader():
   files = glob.glob(datapath)
   files = natsorted(files, key = lambda x: x.lower())
   for i in files:
-    
     filepath = train_images_path + i
     img = np.asarray(Image.open(i))
     img = np.resize(img, (360,480,3))
@@ -27,7 +28,6 @@ def dataloader():
   files = glob.glob(datapath)
   files = natsorted(files, key = lambda x: x.lower()) 
   for i in files:
-
     filepath = train_images_path + i
     img = np.asarray(Image.open(i))
     # img = np.resize(img, (360,480,3))
@@ -35,12 +35,35 @@ def dataloader():
 
   print("y_train loaded!")
 
+  val_images_path = "../data/CamVid/val/"
+  datapath = os.path.join(val_images_path,'*g') 
+  files = glob.glob(datapath)
+  files = natsorted(files, key = lambda x: x.lower()) 
+  for i in files:
+    filepath = train_images_path + i
+    img = np.array(Image.open(i))
+    img = np.resize(img, (360,480,3))
+    X_val.append(img) 
+
+  print("X_val loaded!")
+
+  val_masks_path = "../data/CamVid/val_labels/"
+  datapath = os.path.join(val_masks_path,'*g') 
+  files = glob.glob(datapath)
+  files = natsorted(files, key = lambda x: x.lower()) 
+  for i in files:
+    filepath = train_images_path + i
+    img = np.asarray(Image.open(i))
+    # img = np.resize(img, (360,480,3))
+    y_val.append(img) 
+
+  print("y_val loaded!")
+
   test_images_path = "../data/CamVid/test/"
   datapath = os.path.join(test_images_path,'*g') 
   files = glob.glob(datapath)
   files = natsorted(files, key = lambda x: x.lower()) 
   for i in files:
-
     filepath = train_images_path + i
     img = np.array(Image.open(i))
     img = np.resize(img, (360,480,3))
@@ -53,7 +76,6 @@ def dataloader():
   files = glob.glob(datapath)
   files = natsorted(files, key = lambda x: x.lower()) 
   for i in files:
-
     filepath = train_images_path + i
     img = np.asarray(Image.open(i))
     # img = np.resize(img, (360,480,3))
@@ -63,10 +85,28 @@ def dataloader():
 
   X_train = np.array(X_train)
   y_train = np.array(y_train)
+  X_val = np.array(X_val)
+  y_val = np.array(y_val)
   X_test = np.array(X_test)
   y_test = np.array(y_test) 
 
-  y_train = y_train.reshape((y_train.shape[0], X_train.shape[1]*X_train.shape[2], 12))
-  y_test = y_test.reshape((y_test.shape[0], X_test.shape[1]*X_test.shape[2], 12))
+  y_train = y_train.reshape((y_train.shape[0], X_train.shape[1], X_train.shape[2], 12))
+  y_val = y_val.reshape((y_val.shape[0], X_val.shape[1], X_val.shape[2], 12))
+  y_test = y_test.reshape((y_test.shape[0], X_test.shape[1], X_test.shape[2], 12))
 
-  return X_train, y_train, X_test, y_test
+  return X_train, y_train, X_val, y_val, X_test, y_test
+
+def axis_roll(X_train,X_test,X_val):  
+  X_train = np.rollaxis(X_train[:,:,:,:], axis=3)
+  X_train = np.rollaxis(X_train[:,:,:,:], axis=3)
+  X_train = np.rollaxis(X_train[:,:,:,:], axis=2)
+
+  X_test = np.rollaxis(X_test[:,:,:,:], axis=3)
+  X_test = np.rollaxis(X_test[:,:,:,:], axis=3)
+  X_test = np.rollaxis(X_test[:,:,:,:], axis=2)
+
+  X_val = np.rollaxis(X_val[:,:,:,:], axis=3)
+  X_val = np.rollaxis(X_val[:,:,:,:], axis=3)
+  X_val = np.rollaxis(X_val[:,:,:,:], axis=2)
+
+  return X_train, X_test, X_val
