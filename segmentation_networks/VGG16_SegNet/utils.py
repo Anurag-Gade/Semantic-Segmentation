e1459ea3bb098d2faaf9e5159d12a0d39035e151
Anurag Gade <88252632+Anurag-Gade@users.noreply.github.com>
2023-07-06 13:13:52 -0400

    Update utils.py

--- segmentation_networks/VGG16_SegNet/utils.py
+++ segmentation_networks/VGG16_SegNet/utils.py
@@ -1,13 +1,15 @@
 def import_frameworks():
     import numpy as np
     import matplotlib.pyplot as plt
-    import glob
     import os
-    import cv2
     from natsort import natsorted, ns
     from PIL import Image
+    import cv2
+    import glob
     import datetime
     import tensorflow as tf
     from tensorflow.keras import layers, callbacks, utils
     from tensorflow.keras.models import Model
+    from tensorflow.keras import backend as K
+    from tensorflow.keras.applications import VGG16, VGG19
     
